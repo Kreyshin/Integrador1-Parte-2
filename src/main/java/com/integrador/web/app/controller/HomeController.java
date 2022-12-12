c0305492736eccbfdc2cf67b5fc3d0ce9af4feb2
Kreyshin <kuroshiprince@gmail.com>
2022-12-11 22:59:49 -0500

    Login pt1

---
+++ src/main/java/com/integrador/web/app/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.integrador.web.app.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+    
+    @GetMapping(value="/")
+    public ModelAndView Login(){
+        return new ModelAndView("Login");
+    }
+    
+    @GetMapping(value="/Inicio")
+    public ModelAndView inicio(){
+        return new ModelAndView("Index");
+    }
+
+    @PostMapping(value = "/IniciarSesion")
+    @ResponseBody
+    public Map<String,Object> inicarSesion(String usuario, String clave){
+        Map<String,Object> resultado = new HashMap<>();
+
+        return resultado;
+    }
+
+}
---
+++ src/main/resources/static/Scripts/Sistema/Login.js
@@ -0,0 +1,60 @@
+var validation;
+
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+validation = FormValidation.formValidation(
+	KTUtil.getById('FrmLogin'),
+	{
+		fields: {
+			txt_usuario: {
+				validators: {
+					notEmpty: {
+						message: 'Usuario es necesario'
+					}
+				}
+			},
+			txt_clave: {
+				validators: {
+					notEmpty: {
+						message: 'Clave es necesario'
+					}
+				}
+			}
+		},
+		plugins: {
+            trigger: new FormValidation.plugins.Trigger(),
+            submitButton: new FormValidation.plugins.SubmitButton(),
+            //defaultSubmit: new FormValidation.plugins.DefaultSubmit(), // Uncomment this line to enable normal button submit after form validation
+			bootstrap: new FormValidation.plugins.Bootstrap()
+		}
+	}
+);
+
+$('#BtnIniciarSesion').on('click', function (e) {
+    e.preventDefault();
+
+    validation.validate().then(function(status) {
+        if (status == 'Valid') {
+          $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/IniciarSesion",
+            data: "data",
+            success: function (response) {
+                
+            }
+          });
+        } else {
+            swal.fire({
+                text: "Se encontraron errores en el formulario, por favor intentelo nuevamente.",
+                icon: "error",
+                buttonsStyling: false,
+                confirmButtonText: "OK!",
+                customClass: {
+                    confirmButton: "btn font-weight-bold btn-light-primary"
+                }
+            }).then(function() {
+                KTUtil.scrollTop();
+            });
+        }
+    });
+});
